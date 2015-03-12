1cb4ffaffdf589e4ae6aee836edb3482fd3a47e5
Florian Forster <ff@octo.it>
2015-03-12 14:31:26 +0100

    network: Remove TypesDB parsing, … from parse.go.

--- network/parse.go
+++ network/parse.go
@@ -5,86 +5,15 @@ import (
 	"encoding/binary"
 	"errors"
 	"fmt"
-	"io/ioutil"
-	"strings"
+	"log"
 	"time"
 
 	"collectd.org/api"
 	"collectd.org/cdtime"
 )
 
-const (
-	// Values taken from commit 633c3966f7 of
-	// https://github.com/collectd/collectd/commits/master/src/network.h
-
-	// Notifications
-	ParseMessage  = 0x0100
-	ParseSeverity = 0x0101
-)
-
 var ErrorInvalid = errors.New("Invalid packet")
 var ErrorUnsupported = errors.New("Unsupported packet")
-var ErrorUnknownType = errors.New("Unknown value type")
-var ErrorUnknownDataType = errors.New("Unknown data source type")
-
-var ValueTypeNames = map[string]uint8{
-	"absolute": dsTypeAbsolute,
-	"counter":  dsTypeCounter,
-	"derive":   dsTypeDerive,
-	"gauge":    dsTypeGauge,
-}
-
-var ValueTypeValues = map[uint8]string{
-	dsTypeAbsolute: "absolute",
-	dsTypeCounter:  "counter",
-	dsTypeDerive:   "derive",
-	dsTypeGauge:    "gauge",
-}
-
-type Packet struct {
-	Hostname       string
-	Interval       uint64
-	IntervalHR     uint64
-	Plugin         string
-	PluginInstance string
-	Time           uint64
-	TimeHR         uint64
-	Type           string
-	TypeInstance   string
-	Values         []Value
-}
-
-type Type struct {
-	Name string
-	Type uint8
-	Min  string
-	Max  string
-}
-
-type Types map[string][]*Type
-
-type Value struct {
-	Name     string
-	Type     uint8
-	TypeName string
-	Value    float64
-}
-
-func (p Packet) FormatName() string {
-	metricName := p.Hostname + "/" + p.Plugin
-
-	if len(p.PluginInstance) > 0 {
-		metricName += "-" + p.PluginInstance
-	}
-
-	metricName += "/" + p.Type
-
-	if len(p.TypeInstance) > 0 {
-		metricName += "-" + p.TypeInstance
-	}
-
-	return metricName
-}
 
 // Parse parses the binary network format and returns all
 func Parse(b []byte) ([]api.ValueList, error) {
@@ -226,77 +155,4 @@ func parseString(b []byte) (string, error) {
 
 	buf := bytes.NewBuffer(b[:len(b)-1])
 	return buf.String(), nil
-}
-
-func TypesDBFile(path string) (Types, error) {
-	// See https://collectd.org/documentation/manpages/types.db.5.shtml
-
-	b, err := ioutil.ReadFile(path)
-	if err != nil {
-		return nil, err
-	}
-
-	return TypesDB(b)
-}
-
-func TypesDB(b []byte) (Types, error) {
-	types := make(Types)
-	content := string(b)
-	lines := strings.Split(content, "\n")
-
-	for i, line := range lines {
-		// Skip empty & comment lines
-		if line == "" || line[0] == '#' {
-			continue
-		}
-		dataSetName, dataSetSources, err := ParseDataSet(line)
-		if err != nil {
-			return nil, fmt.Errorf("Line %d: %s", i+1, err.Error())
-		}
-
-		types[dataSetName] = dataSetSources
-	}
-
-	return types, nil
-}
-
-// ParseDataSet parses one line from a collectd types.db file and returns
-// the data-set name & a Type struct
-func ParseDataSet(s string) (string, []*Type, error) {
-	splitFn := func(c rune) bool {
-		return c == '\t' || c == ' ' || c == ','
-	}
-	fields := strings.FieldsFunc(s, splitFn)
-
-	if len(fields) < 2 {
-		return "", nil, fmt.Errorf("Minimum of 2 fields required \"%s\"", s)
-	}
-
-	dataSetName := fields[0]
-	dataSetSources := make([]*Type, len(fields[1:]))
-
-	// Parse each data source
-	for i, field := range fields[1:] {
-		// Split data source fields
-		dsFields := strings.Split(field, ":")
-		if len(dsFields) != 4 {
-			return "", nil, fmt.Errorf("Exactly 4 fields required \"%s\"", field)
-		}
-
-		// Parse data source type
-		dsTypeStr := strings.ToLower(dsFields[1])
-		dsType, ok := ValueTypeNames[dsTypeStr]
-		if !ok {
-			return "", nil, fmt.Errorf("Invalid data-source type \"%s\"", dsTypeStr)
-		}
-
-		dataSetSources[i] = &Type{
-			Name: dsFields[0],
-			Type: dsType,
-			Min:  dsFields[2],
-			Max:  dsFields[3],
-		}
-	}
-
-	return dataSetName, dataSetSources, nil
 }

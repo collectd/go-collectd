15bc2522a900cee9509733da9ac2090c1aee135d
Florian Forster <octo@collectd.org>
2015-08-18 17:06:25 +0200

    cdtime: Turn [Tt]estSerDes() into a table driven test.
    This also removes the old tests, which relied on external "golden data".
    
    Issue: #5

--- cdtime/cdtime_test.go
+++ cdtime/cdtime_test.go
@@ -1,36 +1,14 @@
 package cdtime // import "collectd.org/cdtime"
 
 import (
-	"bytes"
-	"encoding/binary"
-	"encoding/hex"
 	"testing"
 	"time"
 )
 
-func TestDecode(t *testing.T) {
-
-	buf, err := hex.DecodeString("156DB1CFAF08BA7A0009000C")
-	if err != nil {
-		panic(err)
-	}
-
-	var i uint64
-	b := bytes.NewBuffer(buf)
-	if err := binary.Read(b, binary.BigEndian, &i); err != nil {
-		panic(err)
-	}
-
-	hrtime := Time(i)
-
-	want := "2015-07-27 17:05:18.73490774 -0700 PDT"
-	if hrtime.Time().String() != want {
-		t.Errorf("got %v, want %v", hrtime.Time(), want)
-	}
-}
-
-func TestSerDes(t *testing.T) {
-	for _, date := range []string{
+// TestConversion converts a time.Time to a cdtime.Time and back, expecting the
+// original time.Time back.
+func TestConversion(t *testing.T) {
+	cases := []string{
 		"2009-02-04T21:00:57-08:00",
 		"2009-02-04T21:00:57.1-08:00",
 		"2009-02-04T21:00:57.01-08:00",
@@ -40,29 +18,20 @@ func TestSerDes(t *testing.T) {
 		"2009-02-04T21:00:57.000001-08:00",
 		"2009-02-04T21:00:57.0000001-08:00",
 		"2009-02-04T21:00:57.00000001-08:00",
-		"2009-02-04T21:00:57.000000001-08:00"} {
-		testSerDes(t, date)
-	}
-}
-
-func testSerDes(t *testing.T, date string) {
-	t1, err := time.Parse(time.RFC3339Nano, date)
-	if err != nil {
-		panic(err)
+		"2009-02-04T21:00:57.000000001-08:00",
 	}
 
-	t2 := New(t1)
-	if t1.UnixNano() != t2.Time().UnixNano() {
-		t.Errorf("got %s, want %s\n", t2.Time().Format(time.RFC3339Nano), t1.Format(time.RFC3339Nano))
-	}
-}
+	for _, s := range cases {
+		want, err := time.Parse(time.RFC3339Nano, s)
+		if err != nil {
+			t.Errorf("time.Parse(%q): got (%v, %v), want (<time.Time>, nil)", want, err)
+			continue
+		}
 
-func TestNew(t *testing.T) {
-
-	want := time.Unix(1438062739, 123456788)
-	got := New(want).Time()
-
-	if want != got {
-		t.Errorf("got %v, want %v", got, want)
+		cdtime := New(want)
+		got := cdtime.Time()
+		if !got.Equal(want) {
+			t.Errorf("cdtime.Time(): got %v, want %v", got, want)
+		}
 	}
 }
